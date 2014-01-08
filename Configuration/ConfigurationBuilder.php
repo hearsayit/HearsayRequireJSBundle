hearsayit/HearsayRequireJSBundle

commit 51748ca46a0f501f72d16b5f0cbe3f7c1e2677b3

resolve error on cache:clear command: "You cannot create a service ("templating.helperassets") of an inactive scope ("request")

Configuration/ConfigurationBuilder.php
@@ -173,22 +173,19 @@ class ConfigurationBuilder
     protected function getBaseUrl()
     {
         if ($this->container->getParameter('assetic.use_controller')
-            && $this->container->isScopeActive('request')
-        ) {
-            $baseUrl = $this->container->get('request')->getBaseUrl();
-            return $baseUrl;
-        } else {
-            $baseUrl = $this
-                ->container
-                ->get('templating.helper.assets')
-                ->getUrl('');
+            && $this->container->isScopeActive('request')) {
+            return $this->container->get('request')->getBaseUrl();
+        }
 
-            // Remove ?version from the end of the base URL
-            if (($pos = strpos($baseUrl, '?')) !== false) {
-                $baseUrl = substr($baseUrl, 0, $pos);
-                return $baseUrl;
-            }
+        $baseUrl = $this->container->isScopeActive('templating.helper.assets')
+            ? $this->container->get('templating.helper.assets')->getUrl('')
+            : '';
+
+        // Remove version from the end of the base URL
+        if (($pos = strpos($baseUrl, '?')) !== false) {
+            $baseUrl = substr($baseUrl, 0, $pos);
             return $baseUrl;
         }
+        return $baseUrl;
     }
 }

commit 95c6cee0e181e3115d65143e75fa940bac7806fc

Correct Assetic base URL was never being used, fixes bug introduced by 51748ca

Configuration/ConfigurationBuilder.php
@@ -171,18 +171,25 @@ class ConfigurationBuilder
      */
     protected function getBaseUrl()
     {
+        $baseUrl = '';
+
         if ($this->container->getParameter('assetic.use_controller')
             && $this->container->isScopeActive('request')) {
-            return $this->container->get('request')->getBaseUrl();
+            $baseUrl = $this->container->get('request')->getBaseUrl();
         }
 
-        $baseUrl = $this->container->isScopeActive('templating.helper.assets')
-            ? $this->container->get('templating.helper.assets')->getUrl('')
-            : '';
+        if ($assetHelper = $this->container->get('templating.helper.assets')) {
+            $baseUrl = $assetHelper->getUrl('');
 
-        // Remove ?version from the end of the base URL
-        if (($pos = strpos($baseUrl, '?')) !== false) {
-            $baseUrl = substr($baseUrl, 0, $pos);
+            // Remove ?version from the end of the base URL
+            if (($pos = strpos($baseUrl, '?')) !== false) {
+                $baseUrl = substr($baseUrl, 0, $pos);
+            }
+        }
+
+        // Remove trailing slash, if there is one
+        if (substr($baseUrl, -1, 1) === '/') {
+            $baseUrl = substr($baseUrl, 0, -1);
         }
 
         return $baseUrl;